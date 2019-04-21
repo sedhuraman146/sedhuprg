44874c5b4e4bed2317349410d38bae277cc22f40
sedhuraman146 <49307121+sedhuraman146@users.noreply.github.com>
2019-04-21 11:38:07 +0530

    b23.py

---
+++ b23.py
@@ -0,0 +1,12 @@
+def min(list1,n):
+fina1_list=[];
+for i in range(0,n):
+min1=9999999;
+for j in range(len(list1)):
+if list1[j]<min1:
+min1=list[j]
+list1.remove(min1);
+print(final_list)
+list1=(4,78,34,10,8,21,11,231);
+n=2;
+min(list1,n)

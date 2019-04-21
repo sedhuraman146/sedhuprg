9f63699b97812e9512e7f374858619494a1a381a
sedhuraman146 <49307121+sedhuraman146@users.noreply.github.com>
2019-04-21 12:56:42 +0530

    b40.py

---
+++ b40.py
@@ -0,0 +1,12 @@
+def FibonacciNumbers(n):   
+f1 = 0
+f2 = 1
+if (n < 1): 
+return
+for x in range(0, n): 
+print(f2, end = " ") 
+print("")
+next = f1 + f2 
+f1 = f2 
+f2 = next 
+print(FibonacciNumbers(7))

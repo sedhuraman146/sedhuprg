302933395f58f303600d93c110a451d46290f688
sedhuraman146 <49307121+sedhuraman146@users.noreply.github.com>
2019-04-22 20:18:05 +0530

    b72.py

---
+++ b72.py
@@ -0,0 +1,16 @@
+def check(string):
+vowels=set("aeiou")
+s=set({})
+for char in  string:
+if char in vowels:
+s.add(char)
+else:
+pass
+if len(s)==len(vowels):
+print("accepted")
+else:
+print("not accepted")
+if __name__ == "__main__":
+string="Seequial"
+string=string.lower()
+check(string)

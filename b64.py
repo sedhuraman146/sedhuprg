212377cb589b6a9831b783d5f20c6adad1e3f279
sedhuraman146 <49307121+sedhuraman146@users.noreply.github.com>
2019-04-30 09:36:29 +0530

    b64.py

---
+++ b64.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number:")
+b=int(input("enter the number:")
+if((a % 2)==0):
+  print("a is even")
+else:
+  print("a is odd")
+if((b % 2)==0):
+  print("b is even")
+else:
+  print("b is odd")

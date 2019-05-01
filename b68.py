6c291cb4916a535e78f88d56529f629d269a9019
sedhuraman146 <49307121+sedhuraman146@users.noreply.github.com>
2019-05-01 18:28:00 +0530

    b68.py

---
+++ b68.py
@@ -0,0 +1,13 @@
+def multiple(n):
+  if (n<0):
+    return multiple(-n)
+  if(n==0 or n==7):
+    return true
+  if(n<10):
+    return false
+return multiple(n/10-2*(n-n/10*10))
+n=int(input("enter the number:"))
+if(multiple(n):
+  print("yes")
+else:
+  print("no")

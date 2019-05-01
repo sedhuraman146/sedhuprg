f4fd4b208096a3d1409487c5c5ce7074245ceaec
sedhuraman146 <49307121+sedhuraman146@users.noreply.github.com>
2019-05-01 18:49:12 +0530

    b82.py

---
+++ b82.py
@@ -0,0 +1,6 @@
+x = int(input("enter the number:"))
+y = int(input("enter the number:'))
+print("\nOriginal Number: ", x)
+print("Formatted Number with sign: "+"{:+.2f}".x);
+print("Original Number: ", y)
+print("Formatted Number with sign: "+"{:+.2f}".y);

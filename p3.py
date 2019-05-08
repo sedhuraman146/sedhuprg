de66b88802b87f0ddb73a0effa4c8d8b3c18ba6f
sedhuraman146 <49307121+sedhuraman146@users.noreply.github.com>
2019-05-08 13:55:52 +0530

    p3.py

---
+++ p3.py
@@ -0,0 +1,7 @@
+number=int(input("enter the number:"))
+reverse=0
+while(number>0):
+  reminder=number%10
+  reverse=(reverse*10)+reminder
+  number=number//10
+print(" reverse number is=%d"%reverse)
